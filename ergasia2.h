af24345ff8df9d4ca0d63fa2ee01b0da6be514ac
pagaaa18 <pagalidis10@gmail.com>
2025-01-12 19:22:55 +0200

    Update ergasia2.h

--- ergasia2.h
+++ ergasia2.h
@@ -1,6 +1,3 @@
-#ifndef ESHOP_H
-#define ESHOP_H
-
 #define MAX_PRODUCTS 20  // Megistos arithmos proionton
 #define MAX_CLIENTS 5  // Megistos arithmos pelaton
 
@@ -20,5 +17,3 @@ typedef struct {
 void init_catalog(Product catalog[]);
 void process_order(Product catalog[], Order order, int client_fd);
 void print_report(Product catalog[], int total_orders, int successful_orders, int failed_orders, float total_revenue);
-
-#endif // ESHOP_H
